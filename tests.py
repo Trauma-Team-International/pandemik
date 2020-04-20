add2a64bd32d058556b7852a02f10799dc95f3e9
Mikko Kotila <mailme@mikkokotila.com>
2020-04-20 15:09:49 +0300

    added Hofstede+ model

---
+++ tests.py
@@ -0,0 +1,21 @@
+## HofstedePlus.py
+
+# initiate the scoring
+hp = HofstedePlus()
+
+# set custom weights
+hp.set_weights()
+
+# set randomess
+hp.get_score()
+
+# compute the score
+hp.get_score()
+
+# set custom weights and get new result
+hp.set_weights(weight_uncertainty_avoidance=.2)
+hp.get_score()
+
+# set randomness factor and get new result
+hp.set_randomness(factor=.03)
+hp.get_score()
